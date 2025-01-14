bd9e6e6edd37283cd56f8db175abb6df9d2dc0be
nchoquet <nicolachoquet06250@gmail.com>
2025-01-14 16:45:23 +0100

    Fix sur framework, clipboard et norsys

--- .githooks/post-checkout.ps1
+++ .githooks/post-checkout.ps1
@@ -2,22 +2,14 @@ If (-not (Test-Path "$(Get-Location)/bash-aliases-framework")) {
     git clone git@github.com:nicolachoquet06250/bash-aliases-framework.git
 }
 
-rm -rf "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1"
-ln -s "$(Get-Location)/bash-aliases-framework/.githooks/post-checkout.ps1" "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1"
-Set-ExecutionPolicy Bypass -command "$(Get-Location)/bash-aliases-framework/.githooks/post-checkout.ps1"
+Remove-Item -Recurse -Force "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1"
+New-Item -ItemType SymbolicLink -Path "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1" -Target "$(Get-Location)/bash-aliases-framework/.githooks/post-checkout.ps1"
 
-. "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1" "install-script"
+powershell "$(Get-Location)/bash-aliases-framework/.git/hooks/post-checkout.ps1" "install-script"
 
 If (-not (Test-Path "${HOME}/.bash-alias-clipboard")) {
-    $path = ""
-    If ($args[1] -eq "install-script") {
+    If ($args[0] -eq "install-script") {
         $path = "/bash-alias-clipboard"
+        New-Item -ItemType SymbolicLink -Path "${HOME}/.bash-alias-clipboard" -Target "$(Get-Location)${path}"
     }
-    ln -s "$(Get-Location)${path}" "${HOME}/.bash-alias-clipboard";
-}
-
-$source='source "\${HOME}/.bash-alias-clipboard/.aliases.ps1"'
-
-If (-not (Select-String $profile -Pattern $source)) {
-    Set-Content -Path $profile -Value $source
 }
